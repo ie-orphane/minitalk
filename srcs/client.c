51e2a25476bce7c2c901d6dced1e72fec9f89510
ie-orphane <ilyasselyatime0@gmail.com>
2025-03-03 14:08:33 +0000

    bonus: show message in the client when the message recieved by the server

--- srcs/client.c
+++ srcs/client.c
@@ -6,15 +6,13 @@
 /*   By: ielyatim <ielyatim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 13:55:16 by ielyatim          #+#    #+#             */
-/*   Updated: 2025/02/28 11:34:02 by ielyatim         ###   ########.fr       */
+/*   Updated: 2025/03/03 14:04:32 by ielyatim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-int		pid;
-
-void	byte_send(char byte)
+void	byte_send(int pid, char byte)
 {
 	short int	j;
 
@@ -33,17 +31,19 @@ void	byte_send(char byte)
 void	sighandler(int signum)
 {
 	if (signum == SIGUSR1)
-		ft_printf("Message received by server\n");
+		ft_printf("✅ Message received\n");
 }
 
 int	main(int argc, char **argv)
 {
+	int	pid;
+
 	(void)argc;
 	pid = ft_atoi(argv[1]);
 	signal(SIGUSR1, sighandler);
 	while (true)
 	{
-		byte_send(*argv[2]);
+		byte_send(pid, *argv[2]);
 		if (!*argv[2])
 			break ;
 		argv[2]++;
